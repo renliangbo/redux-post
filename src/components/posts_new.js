8f7a50ce7bd0ca616674d8eb5aae361254407395
renliangbo <renna0508@126.com>
2018-01-10 12:58:38 +0800

    add

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,56 +1,77 @@
 import React ,{Component}from'react';
 import {reduxForm,Field ,Fields} from 'redux-form';
-
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
 class PostNew extends Component {
+	submit(){
+		console.log(4)
+	}
 	render(){
 		const {handleSubmit}=this.props;
-
-		const titleField=(field)=>{
-			console.log(field)
+		console.log(this.props)
+		const renderFields=(fields)=>{
+		
 			return (
-				<div className="form-group">
-					<label for="exampleInputEmail1">Email address</label>
-					<input 
-						type="text"
-						name="title"
-						className="form-control"
-						id="exampleInputEmail1"
-						placeholder="Enter title"
-							/>
-					<small id="emailHelp" className="form-text text-muted">
-						We'll never share your email with anyone else.
-					</small>
+				<div>
+					<form onSubmit={handleSubmit(this.submit)}>
+						<div className="form-group">
+							<label for="exampleInputEmail1">title</label>
+							<input 
+								type="text"
+								className="form-control"
+								id="exampleInputEmail1"
+								placeholder="Enter title"
+								{...fields.title.input}
+									/>
+							<small id="emailHelp" className="form-text text-muted">
+								We'll never share your email with anyone else.
+							</small>
+						</div>
+
+						<div className="form-group">
+							<label for="exampleInputPassword1">Categories</label>
+							<input 
+								type="text"
+								className="form-control"
+								id="exampleInputEmail1"
+								placeholder="Enter categories"
+								{...fields.categories.input}
+									/>
+							<small id="emailHelp" className="form-text text-muted">
+								We'll never share your email with anyone else.
+							</small>
+						</div>
+
+						<div className="form-group">
+							<label for="exampleInputPassword">Content</label>
+							<input 
+								type="text"
+								className="form-control"
+								id="exampleInputPassword"
+								placeholder="Password"
+								{...fields.content.input}
+								/>
+						</div>
+						<button  type="submit" className="btn btn-primary">Save</button>
+						<div className="btn btn-primary">Cancle</div>
+
+					</form>
 				</div>
 			)
-		};
+		}
 
 		return(
-			<div>
-				<form onSubmit={handleSubmit}>
-					<Field name="myField" component={titleField}/>
-					<div className="form-group">
-						<label for="exampleInputPassword1">Categories</label>
-						<Field 
-							type="text"
-							name="categories"
-							component="input"
-							className="form-control"
-							id="exampleInputEmail1"
-							placeholder="Enter categories"
-							id="exampleInputPassword1"
-							 />
-					</div>
-					<div className="form-group">
-						<label for="exampleInputPassword">Content</label>
-						<input type="text" className="form-control" id="exampleInputPassword" placeholder="Password"/>
-					</div>
-					<button  type="submit" className="btn btn-primary">Save</button>
-					<div className="btn btn-primary">Cancle</div>
-				</form>
-			</div>
+			<Fields names={[ 'title', 'categories','content' ]} component={renderFields}/>
 		)
 	}
 }
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({createPost},dispatch);
+};
+
+PostNew=connect(null,mapDispatchToProps)(PostNew)
 
 export default reduxForm({
 	form:'PostsNewForm'
